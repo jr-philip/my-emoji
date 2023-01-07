40bf1b498bec9dab3c1c003f068a35fa0ab2ff26
jr-philip <philipmwaura48@gmail.com>
2023-01-06 18:44:22 -0600

    rendering out the emojis

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const myEmoji = ["🧑‍🎓", "🧑‍💻","🕵️‍♂‍"]
+const emojiContainer = document.getElementById("emoji-container")
 
 for(let i = 0; i<myEmoji.length;i++){
-    console.log(myEmoji[i])
+ const emojis = document.createElement('span')
+ emojis.textContent = myEmoji[i]
+ emojiContainer.append(emojis)
 }
