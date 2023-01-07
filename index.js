15da72bb0d13ccf5a020896e8b39886fb9402ff1
jr-philip <philipmwaura48@gmail.com>
2023-01-06 19:14:00 -0600

    adding input btn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ for(let i = 0; i<myEmoji.length;i++){
  emojis.textContent = myEmoji[i]
  emojiContainer.append(emojis)
 }
+
+const emojiBtn = document.getElementById("emoji-btn")
+emojiBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    console.log(emojiInput.value)
+})
